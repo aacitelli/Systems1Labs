c23b49a95ab0e3a6d887a3e0bb713166f4969346
acitelli.2 <acitelli.2@cse-std1.coeit.osu.edu>
2020-01-18 14:24:21 -0500

    Initial commit

---
+++ lab1/lab1.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+
+int main(void) {
+    char string1[] = "The quick fox jumped over the brown dog.";
+    int i; 
+    int max = 1; 
+
+    for (i = 0; i < max; i++) {
+        printf("Hello, World!\n");
+    }
+
+    printf("The sample sentence is \"%s\".\n", string1);
+    return(0);
+}
